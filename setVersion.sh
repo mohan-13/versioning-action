4e03f4e27dd3aaaaf8ae80e47cf42f543594ccd8
MOHANKUMAR T <mohan13081999@gmail.com>
2022-09-09 16:15:58 +0530

    Added version script
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>

---
+++ setVersion.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+checkIfTagExists(){
+  version=$1
+  if [ $(git tag -l "$version") ]; then
+    echo "Error: Version $version already released. Please update your version in package/.appversion"
+    exit 1
+  fi
+}
+
+setVersionAsGithubEnv(){
+  version=$1
+  echo "Setting version $version"
+  echo "ARTIFACT_VERSION=$version" >> $GITHUB_ENV
+}
+
+case $GITHUB_REF in
+  refs/tags/*)
+      echo "Current action is for tag.."
+      setVersionAsGithubEnv "$GITHUB_REF_NAME"
+      ;;
+  refs/heads/release-*)
+      echo "Current action is for release branch.."
+      version=$(echo $GITHUB_REF_NAME | cut -d '-' -f 2)
+      checkIfTagExists "$version"
+      setVersionAsGithubEnv "$version"-rc
+      ;;
+  *)
+      echo "Current action is neither tag nor release branch.."
+      version=$(cat package/.appversion)
+      checkIfTagExists "$version"
+      setVersionAsGithubEnv "$version-$GITHUB_RUN_NUMBER"
+      ;;
+esac
